d1723ca3780ea71fd188897ddfe641d0dca6aa72
changbhc@gmail.com <changbhc@gmail.com>
2019-08-29 15:45:17 -0700

    refactored menu component by removing all functions and changing props passed to arrow functions instead

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -37,156 +37,123 @@ export default function Menu() {
   const [nigiri, setNigiri] = React.useState(false);
   const [sashimi, setSashimi] = React.useState(false);
   const [combination, setCombination] = React.useState(false);
-
-  function handleAppetizer() {
-    setAppetizer(!appetizer)
-  }
-  function handleTempura() {
-    setTempura(!tempura)
-  }
-  function handleUdon() {
-    setUdon(!udon)
-  }
-  function handleCarte() {
-    setCarte(!carte)
-  }
-  function handleMaki() {
-    setMaki(!maki)
-  }
-  function handleTemaki() {
-    setTemaki(!temaki)
-  }
-  function handleNigiri() {
-    setNigiri(!nigiri)
-  }
-  function handleSashimi() {
-    setSashimi(!sashimi)
-  }
-  function handleCombination() {
-    setCombination(!combination)
-  }
-
-    const arr_app = [
-        "Miso Soup",
-        "House Green Salad",
-        "Gyoze", "Edamame",
-        "Harumaki",
-        "Seaweed Salad",
-        "Agedashi Tofu",
-        "Sunomono Salad",
-        "Chicken Karage",
-        "Soft Shell Crab",
-        "Tuna Tataki",
-        "Potato Croquette",
-        "Tuna Carppachio"
-    ]
-    const arr_tempura = [
-        "Prawn Tempura",
-        "Yam Tempura",
-        "Vegetable Tempura",
-        "Assorted Tempura",
-        "Appetizer Tempura",
-        "Sweet Potato Tempura"
-    ]
-    const arr_udon = [
-        "Plain Udon",
-        "Beef Udon",
-        "Chicken Udon",
-        "Seafood Udon",
-        "Beef Yakiudon",
-        "Chicken Yakiudon",
-        "Seafood Yakiudon",
-        "Nabeyaki Udon"
-    ]
-    const arr_carte = [
-        "Chicken Teriyaki",
-        "Beef Teriyaki",
-        "Chicken Curry",
-        "Beef Curry",
-        "Chicken Teriyaki Donburi",
-        "Beef Teriyaki Donburi",
-        "Sable Fish",
-        "Unagi Donburi"
-    ]
-
-    const arr_maki = [
-        "Kappa Roll",
-        "Oshinko Roll",
-        "Salmon Roll",
-        "Tuna Roll",           
-        "Negitoro Roll",
-        "California Roll",
-        "Salmon Avocado Roll",
-        "Yam Tempura Roll",
-        "BC Roll",
-        "Dynamite Roll",
-        "Mango Roll",
-        "Philadelphia Roll",
-        "Unagi Roll",
-        "Chopped Scallop Roll",
-        "Spicy Salmon Roll",
-        "Spicy Tuna Roll"
-    ]
-
-    const arr_temaki = [
-        "Tuna Cone",
-        "Salmon Cone",
-        "Chopped Scallop Cone",
-        "Spicy Tuna Cone",
-        "Spicy Salmon Cone",
-        "Spicy Chopped Scallop Cone"
-    ]
-    const arr_nigiri = [
-        "Inari",
-        "Tamago",
-        "Hokkigai",
-        "Wakame",
-        "Tuna",
-        "Salmon",
-        "Sockeye Salmon",
-        "Masago",
-        "Saba",
-        "Ebi",
-        "Chopped Scallop",
-        "Tobiko",
-        "Tai",
-        "Ika",
-        "Toro",
-        "Tobiko & Quall Egg",
-        "Smoke Salmon",
-        "Tako",
-        "Amaebi",
-        "Hotategai",
-        "Unagi",
-        "Ikura",
-        "Hamachi",
-        "Red Tuna",
-        "Uni"
-    ]
-
-    const arr_sashimi = [
-        "Salmon Sashimi",
-        "Tuna Sashimi",
-        "Spicy Salmon Sashimi",
-        "Spicy Tuna Sashimi",
-        "Tuna & Salmon Sashimi",
-        "Sockeye Salmon Sashimi",
-        "Hokkigai Sashimi",
-        "Toro Sashimi",
-        "Tako Sashimi",
-        "Amaebi Sashimi",
-        "Hamachi Sashimi",
-        "Assorted Sashimi",
-        "Red Tuna Sashimi",
-        "Uni Sashimi"
-    ]
-
-    const arr_combo =[
-        "Party Tray A",
-        "Party Tray B",
-        "Party Tray C",
-        "Spicy Combo"
-    ]
+  const arr_app = [
+    "Miso Soup",
+    "House Green Salad",
+    "Gyoze", "Edamame",
+    "Harumaki",
+    "Seaweed Salad",
+    "Agedashi Tofu",
+    "Sunomono Salad",
+    "Chicken Karage",
+    "Soft Shell Crab",
+    "Tuna Tataki",
+    "Potato Croquette",
+    "Tuna Carppachio"
+  ]
+  const arr_tempura = [
+    "Prawn Tempura",
+    "Yam Tempura",
+    "Vegetable Tempura",
+    "Assorted Tempura",
+    "Appetizer Tempura",
+    "Sweet Potato Tempura"
+  ]
+  const arr_udon = [
+    "Plain Udon",
+    "Beef Udon",
+    "Chicken Udon",
+    "Seafood Udon",
+    "Beef Yakiudon",
+    "Chicken Yakiudon",
+    "Seafood Yakiudon",
+    "Nabeyaki Udon"
+  ]
+  const arr_carte = [
+    "Chicken Teriyaki",
+    "Beef Teriyaki",
+    "Chicken Curry",
+    "Beef Curry",
+    "Chicken Teriyaki Donburi",
+    "Beef Teriyaki Donburi",
+    "Sable Fish",
+    "Unagi Donburi"
+  ]
+  const arr_maki = [
+    "Kappa Roll",
+    "Oshinko Roll",
+    "Salmon Roll",
+    "Tuna Roll",
+    "Negitoro Roll",
+    "California Roll",
+    "Salmon Avocado Roll",
+    "Yam Tempura Roll",
+    "BC Roll",
+    "Dynamite Roll",
+    "Mango Roll",
+    "Philadelphia Roll",
+    "Unagi Roll",
+    "Chopped Scallop Roll",
+    "Spicy Salmon Roll",
+    "Spicy Tuna Roll"
+  ]
+  const arr_temaki = [
+    "Tuna Cone",
+    "Salmon Cone",
+    "Chopped Scallop Cone",
+    "Spicy Tuna Cone",
+    "Spicy Salmon Cone",
+    "Spicy Chopped Scallop Cone"
+  ]
+  const arr_nigiri = [
+    "Inari",
+    "Tamago",
+    "Hokkigai",
+    "Wakame",
+    "Tuna",
+    "Salmon",
+    "Sockeye Salmon",
+    "Masago",
+    "Saba",
+    "Ebi",
+    "Chopped Scallop",
+    "Tobiko",
+    "Tai",
+    "Ika",
+    "Toro",
+    "Tobiko & Quall Egg",
+    "Smoke Salmon",
+    "Tako",
+    "Amaebi",
+    "Hotategai",
+    "Unagi",
+    "Ikura",
+    "Hamachi",
+    "Red Tuna",
+    "Uni"
+  ]
+  const arr_sashimi = [
+    "Salmon Sashimi",
+    "Tuna Sashimi",
+    "Spicy Salmon Sashimi",
+    "Spicy Tuna Sashimi",
+    "Tuna & Salmon Sashimi",
+    "Sockeye Salmon Sashimi",
+    "Hokkigai Sashimi",
+    "Toro Sashimi",
+    "Tako Sashimi",
+    "Amaebi Sashimi",
+    "Hamachi Sashimi",
+    "Assorted Sashimi",
+    "Red Tuna Sashimi",
+    "Uni Sashimi"
+  ]
+  const arr_combo = [
+    "Party Tray A",
+    "Party Tray B",
+    "Party Tray C",
+    "Spicy Combo"
+  ]
   return (
     <List
       component="nav"
@@ -198,7 +165,7 @@ export default function Menu() {
       }
       className={classes.root}
     >
-      <ListItem button onClick={handleAppetizer}>
+      <ListItem button onClick={() => setAppetizer(!appetizer)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -207,10 +174,10 @@ export default function Menu() {
       </ListItem>
       <Collapse in={appetizer} timeout="auto" unmountOnExit>
         {arr_app.map((value) => {
-            return <Item name={value}/> 
+            return <Item name={value}/>
          })}
       </Collapse>
-      <ListItem button onClick={handleTempura}>
+      <ListItem button onClick={() => setTempura(!tempura)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -220,11 +187,11 @@ export default function Menu() {
       <Collapse in={tempura} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_tempura.map((value) => {
-            return <Item name={value}/> 
+            return <Item name={value}/>
          })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleUdon}>
+      <ListItem button onClick={() => setUdon(!udon)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -232,13 +199,13 @@ export default function Menu() {
         {udon ? <ExpandMore /> : <ExpandLess />}
       </ListItem>
       <Collapse in={udon} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding> 
+        <List component="div" disablePadding>
             {arr_udon.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleCarte}>
+      <ListItem button onClick={() => setCarte(!carte)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -248,11 +215,11 @@ export default function Menu() {
       <Collapse in={carte} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_carte.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleMaki}>
+      <ListItem button onClick={() => setMaki(!maki)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -262,11 +229,11 @@ export default function Menu() {
       <Collapse in={maki} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_maki.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleTemaki}>
+      <ListItem button onClick={() => setTemaki(!temaki)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -276,11 +243,11 @@ export default function Menu() {
       <Collapse in={temaki} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_temaki.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleNigiri}>
+      <ListItem button onClick={() => setNigiri(!nigiri)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -290,11 +257,11 @@ export default function Menu() {
       <Collapse in={nigiri} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_nigiri.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleSashimi}>
+      <ListItem button onClick={() => setSashimi(!sashimi)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -304,11 +271,11 @@ export default function Menu() {
       <Collapse in={sashimi} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_sashimi.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
-      <ListItem button onClick={handleCombination}>
+      <ListItem button onClick={() => setCombination(!combination)}>
         <ListItemIcon>
           <InboxIcon />
         </ListItemIcon>
@@ -318,7 +285,7 @@ export default function Menu() {
       <Collapse in={combination} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
             {arr_combo.map((value) => {
-                return <Item name={value}/> 
+                return <Item name={value}/>
             })}
         </List>
       </Collapse>
